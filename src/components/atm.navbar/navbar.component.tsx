79dcb433ffa8b62478c7bb16c9f0d6ed65f381ec
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-10 15:48:33 -0300

    ajustando navbar

--- src/components/atm.navbar/navbar.component.tsx
+++ src/components/atm.navbar/navbar.component.tsx
@@ -6,17 +6,17 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   const IMAGE_WIDTH = "20%";
   return (
-    <nav className="w-full h-32 m-auto bg-violet-900 shadow-md px-8 flex items-center justify-around gap-4">
+    <nav className="sticky top-0 w-full h-32 m-auto bg-violet-900 shadow-md px-8 flex items-center justify-around gap-4">
       <img
         alt={navbarString.altImage}
         width={IMAGE_WIDTH}
         src={navbarString.srcImageLogo}
       />
 
-      <div className="flex items-center gap-4 text-white text-2xl mt-10">
+      <div className="flex items-center gap-4 text-white text-2xl">
         <Link to="/">{navbarString.home}</Link>
-        <Link to="/personagens">{navbarString.Personagens}</Link>
-        <Link to="/episodios">{navbarString.Episodio}</Link>
+        <Link to="/personagens">{navbarString.personagens}</Link>
+        <Link to="/episodios">{navbarString.episodio}</Link>
       </div>
     </nav>
   );
--- src/components/atm.navbar/navbar.string.ts
+++ src/components/atm.navbar/navbar.string.ts
@@ -1,8 +1,8 @@
 export const navbarString = {
   title: "Rick and Morty",
   home: "Inicio",
-  Episodio: "Episódios",
-  Personagens: "Personagens",
+  episodio: "Episódios",
+  personagens: "Personagens",
   srcImageLogo:
     "https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg",
   altImage: "Rick and-morty imagem",
