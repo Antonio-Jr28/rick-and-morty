d3914bdc5dc57b2a0fa2aa1536168766ae67e97c
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-13 11:30:26 -0300

    criando context

---
+++ src/context/epidodes-card.context.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface Episodes {
+    name: string;
+      id: string;
+      airDate: string;
+      characters: {};
+}
+
+export const EpisodesContext = React.createContext<{ episodes: Episodes } | null>(null);
+
+export const useEpisodesContext = () => {
+    const context = React.useContext(EpisodesContext);
+    if (!context) {
+        throw new Error("EpisodesContext must bu used inside Episodes");
+    }
+    return context;
+};
