117b9064a9c0f8048e360a05e1dfad7e2d0821cc
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-13 18:47:18 -0300

    adicionando estilo

--- src/modules/episodes/components/episodes-characters-card.tsx
+++ src/modules/episodes/components/episodes-characters-card.tsx
@@ -8,8 +8,8 @@ export const EpisodesCharactersCard: React.FC = () => {
     <div>
       {episodes?.characters?.length > 0 ? (
         episodes.characters.map((character) => (
-          <div key={character?.id}>
-            <img className="rounded-full	" src={character.image} alt="Personagem" />
+          <div className="flex flex-wrap justify-between p-2" key={character?.id}>
+            <img className="rounded-full w-10 h-10 " src={character.image} alt="Personagem" />
             <p>{character.name}</p>
           </div>
         ))
