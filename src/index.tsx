5a10adaf8bd7bafbd96890cc2c965e9aa98b3a19
AntonioJr-28 <goncalvesantonio-jr@gmail.com>
2023-11-21 18:27:52 -0300

    tetse

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 
-import './index.css';
-import { App } from './App';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-// eslint-disable-next-line react/no-deprecated
-ReactDOM.render(
+import { App } from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
